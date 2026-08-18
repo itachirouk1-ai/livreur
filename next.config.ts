0fe17b9dcccad9c736664239b4d3a978d6105e42
itachirouk1-ai <itachirouk1@gmail.com>
2026-08-18 15:15:25 +0000

    k

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,32 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://connect.facebook.net",
+              "script-src-elem 'self' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com https://connect.facebook.net",
+              "connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com https://region1.google-analytics.com https://region1.googletagmanager.com",
+              "img-src 'self' data: https: https://www.google-analytics.com https://www.googletagmanager.com https://www.facebook.com https://connect.facebook.net",
+              "font-src 'self' data:",
+              "style-src 'self' 'unsafe-inline'",
+              "media-src 'self' data: https:",
+              "frame-src 'self' https://www.google-analytics.com https://www.googletagmanager.com",
+              "object-src 'none'",
+              "base-uri 'self'",
+              "form-action 'self'",
+            ].join('; '),
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
